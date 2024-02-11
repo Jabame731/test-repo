427c1d537c50dfb27f45bca0f5bd0312f261f2d4
Jabame731 <94708537+Jabame731@users.noreply.github.com>
2024-02-11 19:06:41 +0800

    Create bubble.ts

---
+++ bubble.ts
@@ -0,0 +1,22 @@
+ // @if (
+ //      lastMessage.messageSid === message.messageSid &&
+ //      lastMessage.messageId === message.messageId &&
+ //      message.deliveryStatus === 'success'
+ //    ) {
+ //      <p class="text-xs ml-1 text-gray dark:text-gray-400 mt-[0.5px] mr-2">Sent</p>
+ //    }
+
+//  @Input() lastMessage: MessageModel;
+
+//   <ups-phone-text-message-chat-bubble [message]="message" [userNumber]="phoneNumberAssignment.phoneNumber"></ups-phone-text-message-chat-bubble>
+// +      <ups-phone-text-message-chat-bubble [message]="message" [userNumber]="phoneNumberAssignment.phoneNumber"
+// +      
+// +        [lastMessage]="currentMessages[currentMessages.length-1]"
+// +      
+// +      
+// +      ></ups-phone-text-message-chat-bubble>
+//      }
+//    </ul>
+//  </div>
+// +  notificationDeliveryFailed$ = new BehaviorSubject<MessageModel>(null);
+
